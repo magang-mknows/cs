1a53754b10b695dc8ff673615cd2cada270fda40
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-03-26 23:33:18 +0700

    improvement: add skeleton loading

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,7 +3,7 @@ import { TCommon } from "../utilities/entities";
 
 export const BaseLayout: FC<TCommon> = ({ children }): ReactElement => {
   return (
-    <section className="flex flex-col items-center w-full h-screen">
+    <section className="flex flex-col justify-center items-center w-full h-screen">
       {children}
     </section>
   );
--- src/modules/home/loading-home.tsx
+++ src/modules/home/loading-home.tsx
@@ -1,5 +1,7 @@
 import { FC, ReactElement } from "react";
 
 export const LoadingHome: FC = (): ReactElement => {
-  return <div className="bg-gray-100 animate-pulse h-3 w-full" />;
+  return (
+    <div className="bg-gray-200 animate-pulse h-[36px] w-[300px] rounded-lg" />
+  );
 };
