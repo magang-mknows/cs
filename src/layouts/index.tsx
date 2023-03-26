7719a7417f105a8149e959807f615101aff73daf
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-03-26 23:16:31 +0700

    improvement: add layout

---
+++ src/layouts/index.tsx
@@ -0,0 +1,7 @@
+import { FC, ReactElement } from "react";
+
+export const BaseLayout: FC = (): ReactElement => {
+  return (
+    <section className="flex flex-col items-center w-full h-screen"></section>
+  );
+};
