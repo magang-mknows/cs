1948d8082fb600cd17d78643cb276a013fd03f33
Fenny Oktaviani <fnny04@gmail.com>
2023-04-05 17:12:20 +0700

    feature : create history page card

--- src/components/molecules/card/index.tsx
+++ src/components/molecules/card/index.tsx
@@ -14,7 +14,7 @@ const Card: FC<CardProps> = ({
   buttonClassName,
   buttonColor,
   buttonText,
-  buttonTextStyle,
+  buttonTextstyle,
   buttonHref,
   onClick,
 }): ReactElement => {
@@ -36,25 +36,26 @@ const Card: FC<CardProps> = ({
           className={`${className} relative flex auto flex-col cursor-default rounded bg-[#ffffff]`}
           onClick={onClick}
         >
-          <section className="flex w-full ">{children}</section>
           <div className={titleStyle}>
             <h1>{title}</h1>
           </div>
-
-          <div className={bodyClassName}>
-            {hasButton && (
-              <Link to={`${buttonHref}`}>
-                <section className="flex justify-center">
-                  <Button
-                    className={buttonClassName}
-                    color={buttonColor}
-                    textStyle={buttonTextStyle}
-                  >
-                    {buttonText}
-                  </Button>
-                </section>
-              </Link>
-            )}
+          <div className="flex flex-col justify-between w-full h-full">
+            <div className="flex w-full ">{children}</div>
+            <div className={bodyClassName}>
+              {hasButton && (
+                <Link to={`${buttonHref}`}>
+                  <section className="flex justify-end items-end">
+                    <Button
+                      className={buttonClassName}
+                      color={buttonColor}
+                      textStyle={buttonTextstyle}
+                    >
+                      {buttonText}
+                    </Button>
+                  </section>
+                </Link>
+              )}
+            </div>
           </div>
         </div>
       )}
