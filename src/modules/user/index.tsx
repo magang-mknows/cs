a475ad4fc08724702ddc4282bf386f5a5dc23c44
Fenny Oktaviani <fnny04@gmail.com>
2023-04-11 15:43:40 +0700

    tes responsive on phone

--- src/modules/user/index.tsx
+++ src/modules/user/index.tsx
@@ -7,15 +7,15 @@ const UserModules: FC = (): ReactElement => {
 
   return (
     <div className="bg-white w-full h-full my-8 ">
-      <div className="flex justify-between p-8 pt-12">
-        <div className="flex p-2 w-[420px] rounded-md border">
+      <div className="flex flex-col lg:flex-row gap-y-2 justify-between lg:p-8 pt-12">
+        <div className="flex p-2 lg:mx-1 mx-4 lg:w-[420px] w-[300] rounded-md border">
           <h1>Dari : {date}</h1>
           <input type="date" />
           <h1>Sampai : {date}</h1>
           <input type="date" />
         </div>
-        <div>
-          <form className="flex items-center">
+        <div className="flex ">
+          <form className="flex flex-col lg:flex-row gap-y-3 px-8 lg:px-1 items-center">
             <label htmlFor="simple-search" className="sr-only">
               Search
             </label>
@@ -51,7 +51,7 @@ const UserModules: FC = (): ReactElement => {
         </div>
       </div>
       <div className="flex flex-col justify-between">
-        <div className="p-8">
+        <div className="lg:p-8 p-2">
           <Table />
         </div>
         <div className="flex w-full justify-center items-center gap-x-2">
