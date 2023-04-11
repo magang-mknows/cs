b4a5e5e6a69b07259eec8868fcaa5d61aedce835
Fenny Oktaviani <fnny04@gmail.com>
2023-04-11 16:28:32 +0700

    add sekeleton on page dashboard,user and report

--- src/modules/home/loading-home.tsx
+++ src/modules/home/loading-home.tsx
@@ -1,5 +1,19 @@
 import { FC, ReactElement } from "react";
 
 export const LoadingHome: FC = (): ReactElement => {
-  return <div className="bg-gray-200 animate-pulse h-[36px] w-[300px] rounded-lg" />;
+  return (
+    <div className="p-4">
+      <div className="bg-gray-200 gap-4 animate-pulse h-[36px] w-[250px] rounded-lg" />
+      <div className="grid grid-cols-4 py-4">
+        <div className="bg-gray-200 animate-pulse h-[150px] w-[240px] rounded-lg"></div>
+        <div className="bg-gray-200 animate-pulse h-[150px] w-[240px] rounded-lg"></div>
+        <div className="bg-gray-200 animate-pulse h-[150px] w-[240px] rounded-lg"></div>
+        <div className="bg-gray-200 animate-pulse h-[150px] w-[240px] rounded-lg"></div>
+      </div>
+      <div className="flex gap-4">
+        <div className="bg-gray-200 animate-pulse h-[500px] w-[50%] rounded-lg"></div>
+        <div className="bg-gray-200 animate-pulse h-[500px] w-[50%] rounded-lg"></div>
+      </div>
+    </div>
+  );
 };
--- src/pages/report/index.tsx
+++ src/pages/report/index.tsx
@@ -1,12 +1,12 @@
 import { FC, lazy, ReactElement } from "react";
 import SuspenseError from "@/modules/common/suspense-error";
-import Spinner from "@/components/atoms/spinner";
+import { LoadingReport } from "@/modules/report/loading-report";
 
 const ReportModules = lazy(() => import("@/modules/report"));
 
 const ReportPages: FC = (): ReactElement => {
   return (
-    <SuspenseError error={"error cuy"} loading={<Spinner />}>
+    <SuspenseError error={"error cuy"} loading={<LoadingReport />}>
       <ReportModules />
     </SuspenseError>
   );
